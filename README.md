03d08a6cdbcb4735cdb0e3cd811963161790bb14
Tyler Field <tylerrfield@gmail.com>
2014-07-22 21:58:20 -0600

    Updated readme and combined data_sets in run_analysis

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-coursera-clean-data
+#coursera-clean-data
 ===================
 
-Repo for the Coursera Getting and Cleaning Data Class Assignment
+The run_analyis.R script performs the following:
+1. Download accelerometer data from [here] (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+2. Merge the training and test data sets to create a single dataset consisting of only the mean and standard deviation for each measurement. 
+3. Relabels activities and variables with descriptive names.
+4. Creates a new data set with the average of each variable for each activity and each subject and writes it to a file. 
+
+
+Full details of this data set can be obtained from the [site] (http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) where the data was obtained.
+
+###Dependancies
+=============
+The scripts used depend on the following R packages: 
+-curl
--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,23 @@ if (!file.exists('accelerometers.zip')) {
   unzip('accelerometers.zip')
 }
 
-## Work with the Test DataSet
-setwd('UCI HAR Dataset/test/')
-test_set <- read.table('X_test.txt')
+## Extract the names of the coloumns from features.txt
+features <- read.table('UCI HAR Dataset/features.txt', stringsAsFactors = FALSE)
+col_names <- features[,2]
+
+## Combine the Test DataSet
+test_data_set <- read.table('UCI HAR Dataset/test/X_test.txt')
+colnames(test_data_set) <- col_names
+test_data_subjects <- read.table('UCI HAR Dataset/test/y_test.txt')
+colnames(test_data_subjects) <- 'subject'
+test_data <- cbind(test_data_set, test_data_subjects)
+
+## Combine the Train Dataset
+train_data_set <- read.table('UCI HAR Dataset/train/X_train.txt')
+colnames(train_data_set) <- col_names
+train_data_subjects <- read.table('UCI HAR Dataset/train/y_train.txt')
+colnames(train_data_subjects) <- 'subject'
+train_data <- cbind(train_data_set, train_data_subjects)
+
+## Combine the Test and Train data
+combined_data <- rbind(test_data, train_data)
